13a0cacdaf8bebf94517f1551215a192fb93e2a4
Ivan Shepelev <shepelevivan11032006@gmail.com>
2023-06-19 16:37:26 +0300

    Calculator feature

---
+++ calc/index.js
@@ -0,0 +1,54 @@
+const action = getAction();
+const operandA = getOperand('A');
+const operandB = getOperand('B');
+
+const res = calc(action, operandA, operandB);
+
+showResult(action, operandA, operandB, res);
+
+function isValid (operand) {
+
+
+    return isNaN(operand);
+}
+
+function getOperand(operandName) {
+
+    return Number(prompt(`Enter operand ${operandName}`));
+}
+
+function getAction() {
+
+    return prompt('Enter action +, -, *, / ');
+}
+
+function calc(action, a, b) {
+    let res;
+
+    switch (action) {
+        case "+":
+            res = a + b;
+            break
+        case "-":
+            res = a - b;
+            break;
+        case "*":
+            res = a * b;
+            break;
+        case "/" :
+            res = a / b;
+            break;
+        default :
+            alert("Wrong action");
+    }
+    return res;
+}
+
+function showResult(action, a, b, result) {
+
+    if (!isValid(result)) {
+        alert(`${a} ${action} ${b} = ${result}`);
+    } else {
+        alert("wrong operand");
+    }
+}
