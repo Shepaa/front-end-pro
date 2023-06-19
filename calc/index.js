6cd50547fb183bd9491a5cd0fe5cb1309666390f
Ivan Shepelev <shepelevivan11032006@gmail.com>
2023-06-20 00:16:24 +0300

    fix space

--- calc/index.js
+++ calc/index.js
@@ -7,24 +7,18 @@ const res = calc(action, operandA, operandB);
 showResult(action, operandA, operandB, res);
 
 function isValid (operand) {
-
-
     return isNaN(operand);
 }
 
 function getOperand(operandName) {
-
     return Number(prompt(`Enter operand ${operandName}`));
 }
 
 function getAction() {
-
     return prompt('Enter action +, -, *, / ');
 }
-
 function calc(action, a, b) {
     let res;
-
     switch (action) {
         case "+":
             res = a + b;
@@ -45,10 +39,11 @@ function calc(action, a, b) {
 }
 
 function showResult(action, a, b, result) {
-
     if (!isValid(result)) {
+
         alert(`${a} ${action} ${b} = ${result}`);
     } else {
         alert("wrong operand");
     }
 }
+
