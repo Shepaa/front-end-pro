1cfc915fecb1961d4ead73065e0ed4dd29a9580b
Ivan Shepelev <shepelevivan11032006@gmail.com>
2023-07-20 17:34:58 +0300

    fix review

--- todolist.js
+++ todolist.js
@@ -1,11 +1,10 @@
 const btn = document.querySelector("#msgButton");
 btn.addEventListener("click", onBtnClick);
-
+const ul = document.querySelector("#todoList");
+const input = document.querySelector("#msgInput");
 
 function onBtnClick() {
-    const input = document.querySelector("#msgInput");
     if (input.value) {
-        const ul = document.querySelector("#todoList");
         const li = document.createElement("li");
         li.textContent = input.value;
         ul.append(li)
