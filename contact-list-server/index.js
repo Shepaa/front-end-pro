b8c84fc62084fdfe79b4b15b7e942a9791977900
Ivan Shepelev <shepelevivan11032006@gmail.com>
2023-08-15 15:30:09 +0300

    do refactor

--- contact-list-server/index.js
+++ contact-list-server/index.js
@@ -9,10 +9,8 @@ const table = document.querySelector(".table");
 const btn = document.querySelector(".btn");
 const todoAPI = new TodoAPI(todoUrl);
 let contactsList = [];
-
-
 btn.addEventListener('click', onBtnClick);
-
+table.addEventListener('click', onTableClick);
 function onBtnClick() {
     const todo = getTodoData();
     if (isTodoValid(todo)) {
@@ -33,19 +31,81 @@ function getTodoData() {
         phone: phoneInputEl.value,
     }
 }
-function renderList(list) {
-    const html = list.map(generateHtml).join('');
-    table.insertAdjacentHTML(`beforeend`, html)
+
+function isTodoValid(todo) {
+
+    if (!todo.firstName || !todo.lastName || !todo.phone) {
+        showError("Поля не должны быть пустыми");
+        return false;
+    }
+
+    if (isNaN(todo.phone)) {
+        showError("В поле Phone могут быть только цифры");
+        return false;
+    }
+    return true;
 }
+function renderContact(todo) {
+    const contactList = generateHtml(todo);
+    table.insertAdjacentHTML("beforeend", contactList)
+}
+function generateHtml(todo) {
+    return `
+    <tr class="col" data-id="${todo.id}">
+        <td>${todo.firstName}</td>
+        <td>${todo.lastName}</td>
+        <td>${todo.phone}</td>
+        <td><button class="deleteBtn" >Delete</button></td>
+        <td><button class="editBtn">Edit</button></td>
+    </tr>
+`
+}
+
+
+
+
+
+
+
+
 
 
 
 todoAPI.getContactList()
     .then((list) => {
-        contactsList = list;
-        renderList(contactsList);
+        renderList(list);
     })
+function renderList(list) {
+    const html = list.map(generateHtml).join('');
+    table.insertAdjacentHTML(`beforeend`, html)
+}
 
+function onTableClick(e) {
+    if (e.target.classList.contains('deleteBtn')) {
+        onDeleteBtn(e)
+    } else if (e.target.classList.contains('editBtn')) {
+        editData(e);
+    }
+
+}
+function onDeleteBtn(e) {
+    const tr = e.target.closest(".col")
+    const idTr = tr.dataset.id
+    todoAPI.deleteTodoEl(idTr)
+        .then(() => tr.remove())
+        .catch(e => showError(e.message));
+}
+
+function editData(e) {
+    const tr = e.target.closest(".col")
+    const idTr = tr.dataset.id
+    nameInputEl.value = getContactData(tr).name;
+    surnameInputEl.value = getContactData(tr).lastName;
+    phoneInputEl.value = getContactData(tr).phone;
+    btn.removeEventListener('click', onBtnClick);
+    btn.currentId = idTr;
+    btn.addEventListener(`click`, onEditBtnClick);
+}
 
 function getContactData(parent) {
     const name = parent.querySelector('td:nth-child(1)').textContent;
@@ -61,8 +121,6 @@ function getContactData(parent) {
 function onEditBtnClick(e) {
     const updatedTodo = getTodoData();
     const idCol = e.target.currentId;
-
-
     todoAPI.updateContactEl(idCol, updatedTodo).then(() => {
         if (isTodoValid(updatedTodo)) {
             updateContactInTable(idCol, updatedTodo)
@@ -73,7 +131,11 @@ function onEditBtnClick(e) {
 
     })
 }
-
+function clear() {
+    nameInputEl.value = "";
+    surnameInputEl.value = "";
+    phoneInputEl.value = "";
+}
 function updateContactInTable(idCol, updatedTodo) {
     contactsList = document.querySelector(`[data-id~="${idCol}"]`)
     const tdElements = contactsList.querySelectorAll('td')
@@ -82,74 +144,4 @@ function updateContactInTable(idCol, updatedTodo) {
     tdElements[2].textContent = updatedTodo.phone;
 }
 
-
-
-
-function isTodoValid(todo) {
-
-    if (!todo.firstName || !todo.lastName || !todo.phone) {
-        showError("Поля не должны быть пустыми");
-        return false;
-    }
-
-    if (isNaN(todo.phone)) {
-        showError("В поле Phone могут быть только цифры");
-        return false;
-    }
-    return true;
-}
-
-function renderContact(todo) {
-    const contactList = generateHtml(todo);
-
-    table.insertAdjacentHTML("beforeend", contactList)
-}
-
-function onDeleteBtn(e) {
-    const tr = e.target.closest(".col")
-    const idTr = tr.dataset.id
-    todoAPI.deleteTodoEl(idTr)
-        .then(() => tr.remove())
-        .catch(e => showError(e.message));
-}
-
-function generateHtml(todo) {
-    return `
-    <tr class="col" data-id="${todo.id}">
-        <td>${todo.firstName}</td>
-        <td>${todo.lastName}</td>
-        <td>${todo.phone}</td>
-        <td><button class="deleteBtn" >Delete</button></td>
-        <td><button class="editBtn">Edit</button></td>
-    </tr>
-`
-}
-
-table.addEventListener('click', onTableClick);
-
-function onTableClick(e) {
-    if (e.target.classList.contains('deleteBtn')) {
-        onDeleteBtn(e)
-    } else if (e.target.classList.contains('editBtn')) {
-        editData(e);
-    }
-
-}
-
-function editData(e) {
-    const tr = e.target.closest(".col")
-    const idTr = tr.dataset.id
-    nameInputEl.value = getContactData(tr).name;
-    surnameInputEl.value = getContactData(tr).lastName;
-    phoneInputEl.value = getContactData(tr).phone;
-    btn.removeEventListener('click', onBtnClick);
-    btn.currentId = idTr;
-    btn.addEventListener(`click`, onEditBtnClick);
-}
-
-function clear() {
-    nameInputEl.value = "";
-    surnameInputEl.value = "";
-    phoneInputEl.value = "";
-}
 
