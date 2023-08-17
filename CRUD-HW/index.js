daff16e880564412e6ae42c22fa3b5044a999de6
Ivan Shepelev <shepelevivan11032006@gmail.com>
2023-08-17 20:47:23 +0300

    do refactor

--- CRUD-HW/index.js
+++ CRUD-HW/index.js
@@ -7,30 +7,33 @@ const phoneInputEl = document.querySelector(".phoneInput");
 const table = document.querySelector(".table");
 const btn = document.querySelector(".btn");
 const todoAPI = new API(waitersURL);
-let contactsList = [];
+let waitersList = [];
+
 btn.addEventListener('click', onBtnClick);
 table.addEventListener('click', onTableClick);
-// TODO РЕШИТЬ ВОПРОС С АЙДИШКОЙ, У ТЕБЯ ТИПА ЕСТЬ ДВЕ,
-//  И ТИПА ОНИ НЕ ОБНОВЯЛЮТСЯ И МОЖЕТ ОНИ ВООБЩЕ НЕ НУЖНЫ,
-//  СПРОСИ В ГРУППЕ
+
+todoAPI.getList()
+    .then((list) => {
+        renderWaitersList(list)
+    });
+
 function onBtnClick() {
-    const todo = getTodoData();
+    const todo = getWaitersData();
     if (isTodoValid(todo)) {
         todoAPI.createEl(todo)
             .then((newTodo) => {
                 renderWaiters(newTodo);
-                clear()
-
+                clear();
             })
             .catch(e => showError(e.message));
     }
 }
 
-function getTodoData() {
+function getWaitersData() {
     return {
         firstName: nameInputEl.value,
         phone: phoneInputEl.value,
-    }
+    };
 }
 
 function isTodoValid(todo) {
@@ -47,8 +50,8 @@ function isTodoValid(todo) {
 }
 
 function renderWaiters(todo) {
-    const contactList = generateHtml(todo);
-    table.insertAdjacentHTML("beforeend", contactList)
+    const WaitersHtml = generateHtml(todo);
+    table.insertAdjacentHTML("beforeend", WaitersHtml);
 }
 
 function generateHtml(todo) {
@@ -57,52 +60,39 @@ function generateHtml(todo) {
         <td>${todo.firstName}</td>
         <td>${todo.phone}</td>
         <td>${todo.id}</td>
-        
         <td><button class="deleteBtn" >Delete</button></td>
         <td><button class="editBtn">Edit</button></td>
     </tr>
 `
 }
 
-todoAPI.getList()
-    .then((list) => {
-        renderList(list)
-    })
-
-function renderList(list) {
-    const html = list.map(generateHtml).join('');
-
-    table.insertAdjacentHTML(`beforeend`, html)
-}
-
 function onTableClick(e) {
     if (e.target.classList.contains('deleteBtn')) {
-        onDeleteBtn(e)
+        onDeleteBtnClick(e);
     } else if (e.target.classList.contains('editBtn')) {
         fillForm(e);
     }
-
 }
 
-function onDeleteBtn(e) {
-    const tr = e.target.closest(".col")
-    const idTr = tr.dataset.id
+function onDeleteBtnClick(e) {
+    const tr = e.target.closest(".col");
+    const idTr = tr.dataset.id;
     todoAPI.deleteEl(idTr)
         .then(() => tr.remove())
         .catch(e => showError(e.message));
 }
 
 function fillForm(e) {
-    const tr = e.target.closest(".col")
-    const idTr = tr.dataset.id
-    nameInputEl.value = getContactData(tr).name;
-    phoneInputEl.value = getContactData(tr).phone;
+    const tr = e.target.closest(".col");
+    const idTr = tr.dataset.id;
+    nameInputEl.value = getWaitersFormsData(tr).name;
+    phoneInputEl.value = getWaitersFormsData(tr).phone;
     btn.removeEventListener('click', onBtnClick);
     btn.currentId = idTr;
     btn.addEventListener(`click`, saveUpdatedData);
 }
 
-function getContactData(parent) {
+function getWaitersFormsData(parent) {
     const name = parent.querySelector('td:nth-child(1)').textContent;
     const phone = parent.querySelector('td:nth-child(2)').textContent;
 
@@ -113,11 +103,11 @@ function getContactData(parent) {
 }
 
 function saveUpdatedData(e) {
-    const updatedTodo = getTodoData();
+    const updatedTodo = getWaitersData();
     const idCol = e.target.currentId;
     todoAPI.updateEl(idCol, updatedTodo).then(() => {
         if (isTodoValid(updatedTodo)) {
-            updateWaitersInTable(idCol, updatedTodo)
+            updateWaitersInTable(idCol, updatedTodo);
             clear();
             btn.removeEventListener('click', saveUpdatedData);
             btn.addEventListener(`click`, onBtnClick);
@@ -126,16 +116,20 @@ function saveUpdatedData(e) {
     })
 }
 
+function updateWaitersInTable(idCol, updatedTodo) {
+    waitersList = document.querySelector(`[data-id="${idCol}"]`);
+    const tdElements = waitersList.querySelectorAll('td');
+    tdElements[0].textContent = updatedTodo.firstName;
+    tdElements[1].textContent = updatedTodo.phone;
+}
+
 function clear() {
     nameInputEl.value = "";
     phoneInputEl.value = "";
 }
 
-function updateWaitersInTable(idCol, updatedTodo) {
-    contactsList = document.querySelector(`[data-id="${idCol}"]`)
-    const tdElements = contactsList.querySelectorAll('td')
-    tdElements[0].textContent = updatedTodo.firstName;
-    tdElements[1].textContent = updatedTodo.phone;
+function renderWaitersList(list) {
+    const html = list.map(generateHtml).join('');
+
+    table.insertAdjacentHTML(`beforeend`, html);
 }
-
-
