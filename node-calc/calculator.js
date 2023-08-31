2ce039cd275e9d2fd25e3eff4a10db7e515efd4b
Ivan Shepelev <shepelevivan11032006@gmail.com>
2023-08-31 17:23:34 +0300

    do refactor

--- node-calc/calculator.js
+++ node-calc/calculator.js
@@ -3,25 +3,9 @@ const {minusData} = require('./operation/minus');
 const {multiData} = require('./operation/multi');
 const {divData} = require('./operation/div');
 
-function add(a, b) {
-    return plus(a, b);
-}
-
-function minus(a, b) {
-    return minusData(a, b);
-}
-
-function multi(a, b) {
-    return multiData(a, b);
-}
-
-function div(a, b) {
-    return divData(a, b);
-}
-
-module.exports.add = add;
-module.exports.minus = minus;
-module.exports.multi = multi;
-module.exports.div = div;
+module.exports.add = plus;
+module.exports.minus = minusData;
+module.exports.multi = multiData;
+module.exports.div = divData;
 
 
